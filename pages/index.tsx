c9822e83aa142dde399fd3c57d0cead129fb157a
ebenezerakwasi111-netizen <ebenezerakwasi111@gmail.com>
2026-02-21 08:29:35 +0100

    Add Home component with introductory content

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{fontFamily:'sans-serif', maxWidth:'800px', margin:'0 auto', padding:'40px'}}>
+      <h1>Akwasi Forex Learning Hub</h1>
+      <p>Your forex ebook store is coming soon!</p>
+    </div>
+  )
+}
